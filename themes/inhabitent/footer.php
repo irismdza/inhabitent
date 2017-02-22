f6b28e1d2647515830c2a15d5fd58a50a0a7a6d9
Iris <mdza.iris@gmail.com>
2017-02-21 16:16:00 -0800

    Add footer content area.

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -18,5 +18,26 @@
 
 		<?php wp_footer(); ?>
 
+<footer class="footer-inhabitent">
+	<div class="footer-container">
+	<div class="footer-contact">
+		<h3>Contact Info</h3>
+		<p><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></p>
+		<p><a>778-456-7891</a></p>
+	</div>
+	<div class="footer-hours">
+		<h3>Business Hours</h3>
+		<p><span class="day-of-week">Monday-Friday:</span> 9am to 5pm</p>
+		<p><span class="day-of-week">Saturday:</span> 10am to 2pm</p>
+		<p><span class="day-of-week">Sunday:</span> Closed</p>
+	</div>
+	<div class="footer-logo">
+		<img src="../images/logos/inhabitent-tent-dark.svg" />
+	</div>
+	<div class="footer-copyright">
+		<p>Copyright &copy; 2017 Inhabitent</p>
+	</div>
+</footer>
+
 	</body>
 </html>
