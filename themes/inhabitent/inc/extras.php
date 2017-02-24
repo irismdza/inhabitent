81d1ea63b6e48613aa59e749107ca16d11251324
Iris <mdza.iris@gmail.com>
2017-02-23 19:01:04 -0800

    Move new functions into extras.php file.

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -72,3 +72,30 @@ function inhabitent_about_css() {
 	wp_add_inline_style( 'red-starter-style', $hero_css );
 }
 add_action( 'wp_enqueue_scripts', 'inhabitent_about_css');
+
+// Remove "Editor" links from sub-menus
+function inhabitent_remove_submenus() {
+    remove_submenu_page( 'themes.php', 'theme-editor.php' );
+    remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+}
+add_action( 'admin_menu', 'inhabitent_remove_submenus', 110 );
+
+// Sort Product Archive alphabetically
+function sort_product_archive( $query ) {
+    if ( $query->is_post_type_archive('products') && $query->is_main_query() ) {
+		$query->set( 'posts_per_page', 16 );
+        $query->set( 'orderby', 'title' );
+        $query->set( 'order', 'ASC' );
+		return;
+    }
+}
+add_action( 'pre_get_posts', 'sort_product_archive' );
+
+// Remove Product Archive title
+function remove_product_archive_title( $title ) {
+	if ( is_post_type_archive( 'products') ) {
+        $title = 'Shop Stuff';
+	}
+	return $title;
+}
+add_filter( 'get_the_archive_title', 'remove_product_archive_title' );
