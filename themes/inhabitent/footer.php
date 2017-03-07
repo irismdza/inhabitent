ea7a34db58a149dd0258f7a2b49535f4c591c379
Iris <mdza.iris@gmail.com>
2017-03-07 13:39:06 -0800

    Add logo and search form to nav bar.

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -9,40 +9,35 @@
 
 			</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
+			<footer id="colophon" class="site-footer footer-inhabitent" role="contentinfo">
+				<div class="footer-container">
+					<div class="footer-contact">
+						<h3>Contact Info</h3>
+						<p><i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
+						</p>
+						<p><i class="fa fa-phone" aria-hidden="true"></i> <a>778-456-7891</a>
+						</p>
+						<p><i class="fa fa-facebook-square" aria-hidden="true"></i> <i class="fa fa-twitter-square" aria-hidden="true"></i> <i class="fa fa-google-plus-square" aria-hidden="true"></i></p>
+					</div>
+					<div class="footer-hours">
+						<h3>Business Hours</h3>
+						<p><span class="day-of-week">Monday-Friday:</span> 9am to 5pm</p>
+						<p><span class="day-of-week">Saturday:</span> 10am to 2pm</p>
+						<p><span class="day-of-week">Sunday:</span> Closed</p>
+					</div>
+					<div class="footer-logo">
+						<img src="<?php echo get_template_directory_uri() ?>/images/logos/inhabitent-logo-text.svg" />
+					</div>
+					<div class="footer-copyright">
+						<p>Copyright &copy; 2017 Inhabitent</p>
+					</div>
+				</div>
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
-
 		<?php wp_footer(); ?>
-
-<footer class="footer-inhabitent">
-	<div class="footer-container">
-		<div class="footer-contact">
-			<h3>Contact Info</h3>
-			<p><i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
-			</p>
-			<p><i class="fa fa-phone" aria-hidden="true"></i> <a>778-456-7891</a>
-			</p>
-			<p><i class="fa fa-facebook-square" aria-hidden="true"></i> <i class="fa fa-twitter-square" aria-hidden="true"></i> <i class="fa fa-google-plus-square" aria-hidden="true"></i></p>
-		</div>
-		<div class="footer-hours">
-			<h3>Business Hours</h3>
-			<p><span class="day-of-week">Monday-Friday:</span> 9am to 5pm</p>
-			<p><span class="day-of-week">Saturday:</span> 10am to 2pm</p>
-			<p><span class="day-of-week">Sunday:</span> Closed</p>
-		</div>
-		<div class="footer-logo">
-			<img src="<?php echo get_template_directory_uri() ?>/images/logos/inhabitent-logo-text.svg" />
-		</div>
-		<div class="footer-copyright">
-			<p>Copyright &copy; 2017 Inhabitent</p>
-		</div>
-	</div>
-
-</footer>
-
+		<script src="js/jquery-3.1.1.min.js"></script>
 	</body>
 </html>
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -21,15 +21,24 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
+				
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
-
+				
 				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<a href="<?php echo home_url(); ?>">	</a>
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<div class="nav-container">
+						<a href="<?php get_home_url(); ?>" class="logo"></a>
+						<div class="nav-left-menu">
+							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+							<?php echo get_search_form(); ?>
+						</div>
+					</div>
 				</nav><!-- #site-navigation -->
+			
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
