41c190917e700ca43c4ca83ddb8cd35ffc7bea72
Iris <mdza.iris@gmail.com>
2017-03-08 18:25:00 -0800

    Fix contact info widget icon spacing.

--- plugins/inhabitent-contact-info/views/widget.php
+++ plugins/inhabitent-contact-info/views/widget.php
@@ -1,23 +1,34 @@
 <!-- This file is used to markup the public-facing widget. -->
 <?php if ( strlen( trim( $phone ) ) > 0 ) : ?>
-    <p>
-        <i class="fa fa-phone" aria-hidden="true"></i>
-        <a><?php echo $phone; ?></a>
-    </p>
+    <div class="contact-phone">
+        <div class="contact-info-icon">
+            <i class="fa fa-phone" aria-hidden="true"></i>
+        </div>
+        <div>
+            <a><?php echo $phone; ?></a>
+        </div>
+    </div>
 <?php endif; ?>
 
 <?php if ( strlen( trim( $email ) ) > 0 ) : ?>
-    <p>
-        <i class="fa fa-envelope" aria-hidden="true"></i>
-        <a><?php echo $email; ?></a>
-    </p>
+    <div class="contact-email">
+        <div class="contact-info-icon">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+        </div>
+        <div>
+            <a><?php echo $email; ?></a>
+        </div>
+    </div>
 <?php endif; ?>
 
 <?php if ( strlen( trim( $street_address ) ) > 0 ) : ?>
-    <p>
-        <i class="fa fa-map-marker" aria-hidden="true"></i>
-        <?php echo $street_address; ?>
-            <br>
-            <?php echo $city; ?>, <?php echo $province; ?> <?php echo $postal_code; ?>
-    </p>
+    <div class="contact-address">
+        <div class="contact-info-icon">
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+        </div>
+        <div>
+            <p><?php echo $street_address; ?></p>
+            <p><?php echo $city; ?>, <?php echo $province; ?> <?php echo $postal_code; ?></p>
+        </div>
+    </div>
 <?php endif; ?>
